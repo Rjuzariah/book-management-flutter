7268f72bde060e647cb03896a651d5bee00ce3ae
Juzariah <riajuzariah@gmail.com>
2024-10-20 02:20:32 +0700

    applying theme

--- book_management_flutter/lib/main.dart
+++ book_management_flutter/lib/main.dart
@@ -3,10 +3,14 @@ import './screen/book_list.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Book Management',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
     home: BookListPage(),
+    theme: ThemeData(
+      primarySwatch: Colors.blue, // Use primarySwatch to define a range of colors
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.blue, // Set AppBar color globally
+        titleTextStyle: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    ),
   ));
 }
